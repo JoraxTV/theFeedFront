93b5a3fc9e77d4a2bd65049aefb3aa07f6fced5d
JoraxTV <jojodu34dadada@gmail.com>
2023-11-13 16:57:16 +0100

    Exercice 5

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
- import Feed from '@/views/Feed.vue'
- import Users from '@/views/AllUsers.vue'
+// Routeur (ex: router.ts)
+import { createRouter, createWebHistory } from 'vue-router';
+import Feed from '@/views/Feed.vue';
+import Users from '@/views/AllUsers.vue';
+import SingleMessage from '@/views/SingleMessage.vue';
+import SingleUsers from '@/views/SingleUsers.vue';
 
- const router = createRouter({
-     history: createWebHistory(''),
-     routes: [
-         {
-             path: '/',
-             name: 'feed',
-             component: Feed
-         },
-         {
-             path: '/feed',
-             name: 'feed',
-             component: Feed
-         },
-          {
-              path: '/users',
-              name: 'users',
-              component: Users
-          }
-     ]
- })
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Feed,
+      name: 'feed',
+    },
+    {
+      path: '/feed',
+      name: 'feed',
+      component: Feed,
+    },
+    {
+      path: '/users',
+      name: 'users',
+      component: Users,
+    },
+    {
+      path: '/feed/:id',
+      name: 'singleMessage',
+      component: SingleMessage,
+    },
+    {
+      path: '/users/:id',
+      name: 'singleUsers',
+      component: SingleUsers,
+    }
+  ],
+});
 
- export default router
+export default router;
---
+++ src/views/SingleUsers.vue
@@ -0,0 +1,29 @@
+<!-- Chemin : views/SingleMessage.vue -->
+
+<template>
+    <div>
+      <h1>J'affiche l'utilisateur d'id : {{ id }}</h1>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, onMounted } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  const route = useRoute();
+const id = ref<string>('');
+  
+  onMounted(() => {
+    // Récupérer le paramètre id de la route
+    if (Array.isArray(route.params.id)) {
+        id.value = route.params.id[0];
+    } else {
+        id.value = route.params.id;
+    }
+  });
+  </script>
+  
+  <style scoped>
+  /* Styles CSS spécifiques à SingleMessage.vue si nécessaire */
+  </style>
+  
